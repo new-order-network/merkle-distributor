9f71ac647cc1e2b9d33ba1e19d46ec7885b85d38
marek-new <93398596+marek-new@users.noreply.github.com>
2022-06-26 15:26:44 -0500

    Update IMerkleDistributor.sol
    changed claimed function to accept an address instead of a positional index

--- contracts/interfaces/IMerkleDistributor.sol
+++ contracts/interfaces/IMerkleDistributor.sol
@@ -8,7 +8,7 @@ interface IMerkleDistributor {
     /// Returns the merkle root of the merkle tree containing account balances available to claim.
     function merkleRoot() external view returns (bytes32);
     /// Returns the amount of airdrops claimed.
-    function claimed(uint256 index) external view returns (uint256);
+    function claimed(address index) external view returns (uint256);
     /// Claim the given amount of the token to the given address. Reverts if the inputs are invalid.
     /// @param amount the TOTAL eligible amount of airdop in MerkleTree (including claimed amount).
     function claim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof) external;
